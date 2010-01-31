571c493f552b2ba78588d25e38d64cb3cd2676f7
Joe Bauser <coderjoe@coderjoe.net>
2010-01-30 23:34:15 -0500

    Added basic skeleton for application layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,12 @@ class PagesController < ApplicationController
   def home
   end
 
+  def about
+  end
+
+  def contact
+  end
+
+  def projects
+  end
 end
--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -8,4 +8,28 @@ class PagesControllerTest < ActionController::TestCase
 
     should_respond_with :success
   end
+
+  context "on GET to :contact" do
+    setup do
+      get :contact
+    end
+
+    should_respond_with :success
+  end
+
+  context "on GET to :projects" do
+    setup do
+      get :projects
+    end
+      
+    should_respond_with :success
+  end
+
+  context "on GET to :about" do
+    setup do
+      get :about
+    end
+
+    should_respond_with :success
+  end
 end
