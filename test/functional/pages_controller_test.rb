ece13d35e06f9abcb049276359c4d211be98d1f3
Joe Bauser <coderjoe@coderjoe.net>
2010-02-01 23:36:00 -0500

    Adding routing tests

--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -8,4 +8,15 @@ class PagesControllerTest < ActionController::TestCase
 
     should_respond_with :success
   end
+
+  context "on GET to '/home'" do
+    should_route :get, '/home', :controller => :pages, :action => :home
+  end
+
+  context "on GET to '/'" do
+    should "route '/' to the home page" do
+      assert_recognizes({:controller => 'pages', :action => 'home'}, '/')
+    end
+  end
+
 end
