6f6e34b27477b6d31589621121577e03d6f60152
Joe Bauser <coderjoe@coderjoe.net>
2011-07-03 14:15:37 -0400

    Info and Resume mocked

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 JosephbauserNet::Application.routes.draw do
+  get "resume", :to => 'resume#index'
+  get "projects", :to => 'projects#index'
+  get "info", :to => 'home#index'
+
   root :to => 'home#index'
 end
